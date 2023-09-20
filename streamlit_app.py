be3ffef8d0d81ff624e52e6818f5b1b9e2fccc66
Premalsinh <88768581+Premalsinh@users.noreply.github.com>
2023-09-20 14:21:07 +0530

    Update streamlit_app.py
    insert default val to database table

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#temp
+my_cur.execute=("insert into fruit_load_list values ('from streamlit')")
